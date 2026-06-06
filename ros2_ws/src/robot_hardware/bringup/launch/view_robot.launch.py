e1beee5fda884190532cabcafd4e30b7e40ff348
Aaron Yoon <awesomeyooner@gmail.com>
2026-06-06 08:32:52 +0000

    fixed Rviz default path

--- ros2_ws/src/robot_hardware/bringup/launch/view_robot.launch.py
+++ ros2_ws/src/robot_hardware/bringup/launch/view_robot.launch.py
@@ -79,7 +79,7 @@ def generate_launch_description():
     robot_description = {"robot_description": robot_description_content}
 
     rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare(description_package), "description/rviz", "rviz.rviz"]
+        [FindPackageShare(description_package), "rviz", "default.rviz"]
     )
 
     joint_state_publisher_node = Node(
